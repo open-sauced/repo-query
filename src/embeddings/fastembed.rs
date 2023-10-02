28f499a8c913d7d22d9257fb8d34228a7beab2af
Anush008 <46051506+Anush008@users.noreply.github.com>
2023-10-02 21:35:50 +0530

    feat: fastembed for embeddings generation

---
+++ src/embeddings/fastembed.rs
@@ -0,0 +1,25 @@
+use crate::prelude::*;
+use fastembed::{EmbeddingBase, FlagEmbedding};
+
+use super::{Embeddings, EmbeddingsModel};
+
+pub struct Fastembed {
+    model: FlagEmbedding,
+}
+
+impl Fastembed {
+    pub fn try_new() -> Result<Self> {
+        let model = FlagEmbedding::try_new(Default::default())?;
+        Ok(Self { model })
+    }
+}
+
+impl EmbeddingsModel for Fastembed {
+    fn embed<S: AsRef<str> + Send + Sync>(&self, texts: Vec<S>) -> Result<Vec<Embeddings>> {
+        self.model.embed(texts, None)
+    }
+
+    fn query_embed<S: AsRef<str> + Send + Sync>(&self, query: S) -> Result<Embeddings> {
+        self.model.query_embed(query)
+    }
+}
